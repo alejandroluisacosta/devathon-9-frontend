dfc2dfa5b84352ee62a8199cb534b5e741ed76b9
Alejandro <alejandroluisacosta95@gmail.com>
2025-03-29 16:28:02 +0100

    add: background image

---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import './Main.scss';
+
+export const Main = () => {
+  return (
+    <div className="main-page">
+      <h1>Main Page</h1>
+      <p>This is the main page of the application.</p>
+    </div>
+  );
+}
