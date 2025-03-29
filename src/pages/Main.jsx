d2b25c213fb9915c41463a28478ca55c3aea7343
Alejandro <alejandroluisacosta95@gmail.com>
2025-03-29 18:02:12 +0100

    add: list of houses

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,8 +3,15 @@ import './Main.scss';
 export const Main = () => {
   return (
     <div className="main-page">
-      <h1 className='main-page__title'>Player's Name</h1>
+      <h1 className="main-page__player-name">Player's Name</h1>
       <input type="text" className='main-page__name-input' placeholder='Dumbledore' />
+      <h1 className="main-page__pick-house">Pick your house</h1>
+      <div className="main-page__houses">
+        <img className='main-page__houses__logo' src="/images/Gryffindor.webp" alt="Gryffindor" />
+        <img className='main-page__houses__logo' src="/images/Slytherin.webp" alt="Slytherin" />
+        <img className='main-page__houses__logo main-page__houses__logo--hufflepuff' src="/images/Hufflepuff.webp" alt="Huffepuff" />
+        <img className='main-page__houses__logo' src="/images/Ravenclaw.webp" alt="Ravenclaw" />
+      </div>
     </div>
   );
 }
