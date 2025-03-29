7744dc2c69b424f94b0822e43f137692924e03df
Alejandro <alejandroluisacosta95@gmail.com>
2025-03-29 19:24:26 +0100

    add: name of the game

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,9 +3,10 @@ import './Main.scss';
 export const Main = () => {
   return (
     <div className="main-page">
-      <h1 className="main-page__player-name">Player's Name</h1>
+      <h1 className="main-page__title">Expelliarmicus</h1>
+      <h2 className="main-page__player-name">Player's Name</h2>
       <input type="text" className='main-page__name-input' placeholder='Dumbledore' />
-      <h1 className="main-page__pick-house">Pick your house</h1>
+      <h2 className="main-page__pick-house">Pick your house</h2>
       <div className="main-page__houses">
         <img className='main-page__houses__logo' src="/images/Gryffindor.webp" alt="Gryffindor" />
         <img className='main-page__houses__logo' src="/images/Slytherin.webp" alt="Slytherin" />
