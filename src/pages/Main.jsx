87416bd1e54607c9715f7338d4dd18c6cb61a04c
Alejandro <alejandroluisacosta95@gmail.com>
2025-03-30 17:31:24 +0200

    add: "confirmar" button renders the alternative main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { sendPlayerDataToServer } from '../utils/sendPlayerDataToServer';
+import { houseTraits } from '../constants/houseTraits';
 import './Main.scss';
 
 export const Main = () => {
   const [playerName, setPlayerName] = useState("");
   const [selectedHouse, setSelectedHouse] = useState("");
+  const [isPlayerInfoLoaded, setIsPlayerInfoLoaded] = useState(false);
 
   useEffect(() => {
     const storedPlayer = localStorage.getItem("playerInfo");
@@ -12,52 +14,70 @@ export const Main = () => {
       const { name, house } = JSON.parse(storedPlayer);
       setPlayerName(name);
       setSelectedHouse(house);
+      setIsPlayerInfoLoaded(true);
     }
   }, []);
   
   const handleNameChange = (e) => setPlayerName(e.target.value);
   
   const handleHouseSelect = (house) => setSelectedHouse(house);
+
+  const handlePlay = () => {
+
+  }
   
-  const handleSubmit = () => {
+  const handleConfirm = () => {
     const playerData = { name: playerName, house: selectedHouse };
     sendPlayerDataToServer(playerData);
     localStorage.setItem("playerInfo", JSON.stringify(playerData));
-    
+    setIsPlayerInfoLoaded(true);
   };
-  console.log(`Player Name: ${playerName}`);
+
   return (
     <div className="main-page">
       <h1 className="main-page__title">Expelliarmicus</h1>
-      
-      <input 
-        type="text" 
-        className="main-page__name-input" 
-        placeholder="Dumbledore" 
-        value={playerName} 
-        onChange={handleNameChange} 
-      />
-      
-      <h2 className="main-page__pick-house">Pick your house</h2>
-      <div className="main-page__houses">
-        {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
-          <div 
-            key={house} 
-            className="main-page__houses__house"
-            onClick={() => handleHouseSelect(house)}
-            style={{ backgroundColor: selectedHouse === house ? 'lightgrey' : 'transparent' }}  // Optional style for selection
-          >
-            <img 
-              className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
-              src={`/images/${house}.webp`} 
-              alt={house} 
-            />
-            <h3 className="main-page__houses__name">{house}</h3>
-          </div>
-        ))}
-      </div>
-      
-      <button className="main-page__button" onClick={handleSubmit}>Play</button>
+      {isPlayerInfoLoaded ? 
+      <>
+        <h1>{playerName}, el {houseTraits[selectedHouse]}</h1>
+        <div className="main-page__house">
+          <img className={`main-page__houses__logo main-page__houses__logo--${selectedHouse.toLowerCase()}`} src={`/images/${selectedHouse}.webp`} alt={selectedHouse} />
+          <h3 className="main-page__houses__name">{selectedHouse}</h3>
+        </div>
+      </>
+      :
+      <>
+        
+        <input 
+          type="text" 
+          className="main-page__name-input" 
+          placeholder="Dumbledore" 
+          value={playerName} 
+          onChange={handleNameChange} 
+        />
+        
+        <h2 className="main-page__pick-house">Pick your house</h2>
+        <div className="main-page__houses">
+          {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
+            <div 
+              key={house} 
+              className="main-page__houses__house"
+              onClick={() => handleHouseSelect(house)}
+              style={{ backgroundColor: selectedHouse === house ? 'lightgrey' : 'transparent' }}  // Optional style for selection
+            >
+              <img 
+                className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
+                src={`/images/${house}.webp`} 
+                alt={house} 
+              />
+              <h3 className="main-page__houses__name">{house}</h3>
+            </div>
+          ))}
+        </div>
+        
+      </>}
+      <button className="main-page__button" onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}>
+        {isPlayerInfoLoaded ? 'Jugar' : 'Confirmar'}
+      </button>
     </div>
   );
 };
