fffbf34ccae335e5dc6154134a07cb57675314f3
Alejandro Luis Acosta <98220394+alejandroluisacosta@users.noreply.github.com>
2025-04-01 17:55:47 +0200

    Feature: Main page's transition  (#3)
    * add: background image
    
    * add: name input
    
    * add: list of houses
    
    * add: play button
    
    * add: desktop styles
    
    * add: name of the game
    
    * add: scss
    
    * add: house's names
    
    * add: storage of the player's information on localStorage
    
    * add: retrieve data from localStorage to the state of the main component after a page reload
    
    * add: prepare connection to backend
    
    * add: App.css file
    
    * add: "confirmar" button renders the alternative main page
    
    * add: basic player info component styles - mobile
    
    * add: styles for selected house
    
    * add: transition to player's basic info page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { sendPlayerDataToServer } from '../utils/sendPlayerDataToServer';
 import './Main.scss';
+import { PlayerBasicInfo } from '../components/PlayerBasicInfo';
 
 export const Main = () => {
   const [playerName, setPlayerName] = useState("");
   const [selectedHouse, setSelectedHouse] = useState("");
+  const [isPlayerInfoLoaded, setIsPlayerInfoLoaded] = useState(false);
 
   useEffect(() => {
     const storedPlayer = localStorage.getItem("playerInfo");
@@ -12,52 +14,71 @@ export const Main = () => {
       const { name, house } = JSON.parse(storedPlayer);
       setPlayerName(name);
       setSelectedHouse(house);
+      setIsPlayerInfoLoaded(true);
     }
   }, []);
   
   const handleNameChange = (e) => setPlayerName(e.target.value);
   
   const handleHouseSelect = (house) => setSelectedHouse(house);
+
+  const handlePlay = () => {
+
+  }
   
-  const handleSubmit = () => {
+  const handleConfirm = () => {
     const playerData = { name: playerName, house: selectedHouse };
     sendPlayerDataToServer(playerData);
     localStorage.setItem("playerInfo", JSON.stringify(playerData));
-    
+    setIsPlayerInfoLoaded(true);
   };
-  console.log(`Player Name: ${playerName}`);
+
   return (
-    <div className="main-page">
-      <h1 className="main-page__title">Expelliarmicus</h1>
-      
-      <input 
-        type="text" 
-        className="main-page__name-input" 
-        placeholder="Dumbledore" 
-        value={playerName} 
-        onChange={handleNameChange} 
-      />
-      
-      <h2 className="main-page__pick-house">Pick your house</h2>
-      <div className="main-page__houses">
-        {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
-          <div 
-            key={house} 
-            className="main-page__houses__house"
-            onClick={() => handleHouseSelect(house)}
-            style={{ backgroundColor: selectedHouse === house ? 'lightgrey' : 'transparent' }}  // Optional style for selection
-          >
-            <img 
-              className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
-              src={`/images/${house}.webp`} 
-              alt={house} 
-            />
-            <h3 className="main-page__houses__name">{house}</h3>
-          </div>
-        ))}
-      </div>
-      
-      <button className="main-page__button" onClick={handleSubmit}>Play</button>
+    <div className={`main-page ${isPlayerInfoLoaded ? "with-background" : ""}`}>
+      <h1 className="main-page__title relative-element">Expelliarmicus</h1>
+      {isPlayerInfoLoaded ? 
+      <>
+        <PlayerBasicInfo playerName={playerName} selectedHouse={selectedHouse} className="fade-in relative-element"/>
+      </>
+      :
+      <div className={"main-page__form relative-element"}>
+        <input 
+          type="text" 
+          className="main-page__name-input" 
+          placeholder="Dumbledore" 
+          value={playerName} 
+          onChange={handleNameChange} 
+        />
+        
+        <h2 className="main-page__pick-house">Pick your house</h2>
+        <div className="main-page__houses">
+          {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
+            <div 
+              key={house} 
+              className="main-page__houses__house"
+              onClick={() => handleHouseSelect(house)}
+              style={{ 
+                boxShadow: selectedHouse === house ? '0 4px 40px rgba(0, 0, 0, 0.8)' : 'none',
+                backgroundColor: selectedHouse === house ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
+                border: selectedHouse === house ? '1px solid rgb(135, 135, 135)' : 'none',
+                transform: selectedHouse === house ? 'scale(1.05)' : 'scale(1)',
+                transition: 'all 0.3s ease-in-out'
+              }}
+              
+            >
+              <img 
+                className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
+                src={`/images/${house}.webp`} 
+                alt={house} 
+              />
+              <h3 className="main-page__houses__name">{house}</h3>
+            </div>
+          ))}
+        </div>
+      </div>}
+      <button className="main-page__button relative-element" onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}>
+        {isPlayerInfoLoaded ? 'Jugar' : 'Confirmar'}
+      </button>
     </div>
   );
 };
