72d6eb10daa9cfa432cfcb0df971e01023b63f96
Alejandro Luis Acosta <98220394+alejandroluisacosta@users.noreply.github.com>
2025-04-04 22:51:48 +0200

    Feature: Duel room (#5)
    * add: background image
    
    * add: name input
    
    * add: list of houses
    
    * add: play button
    
    * add: desktop styles
    
    * add: name of the game
    
    * add: scss
    
    * add: house's names
    
    * add: storage of the player's information on localStorage
    
    * add: retrieve data from localStorage to the state of the main component after a page reload
    
    * add: prepare connection to backend
    
    * add: App.css file
    
    * add: "confirmar" button renders the alternative main page
    
    * add: basic player info component styles - mobile
    
    * add: styles for selected house
    
    * add: transition to player's basic info page
    
    * add: initial duel room
    
    ---------
    
    Co-authored-by: Bolivar Delgado <bolivar.delgado@gmail.com>

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,8 +4,10 @@ import './Main.scss';
 import { PlayerBasicInfo } from '../components/PlayerBasicInfo';
 
 export const Main = () => {
-  const [playerName, setPlayerName] = useState('');
-  const [selectedHouse, setSelectedHouse] = useState('');
+
+  const [playerName, setPlayerName] = useState("");
+  const [selectedHouse, setSelectedHouse] = useState("");
+
   const [isPlayerInfoLoaded, setIsPlayerInfoLoaded] = useState(false);
 
   useEffect(() => {
@@ -17,71 +19,69 @@ export const Main = () => {
       setIsPlayerInfoLoaded(true);
     }
   }, []);
+  
+  const handleNameChange = (e) => setPlayerName(e.target.value);
+  
+  const handleHouseSelect = (house) => setSelectedHouse(house);
 
-  const handleNameChange = e => setPlayerName(e.target.value);
+  const handlePlay = () => {
 
-  const handleHouseSelect = house => setSelectedHouse(house);
-
-  const handlePlay = () => {};
-
+  }
+  
   const handleConfirm = () => {
     const playerData = { name: playerName, house: selectedHouse };
     sendPlayerDataToServer(playerData);
-    localStorage.setItem('playerInfo', JSON.stringify(playerData));
+    localStorage.setItem("playerInfo", JSON.stringify(playerData));
+
     setIsPlayerInfoLoaded(true);
   };
 
   return (
-    <div className={`main-page ${isPlayerInfoLoaded ? 'with-background' : ''}`}>
-      <h1 className='main-page__title relative-element'>Expelliarmicus</h1>
-      {isPlayerInfoLoaded ? (
-        <>
-          <PlayerBasicInfo
-            playerName={playerName}
-            selectedHouse={selectedHouse}
-            className='fade-in relative-element'
-          />
-        </>
-      ) : (
-        <div className={'main-page__form relative-element'}>
-          <input
-            type='text'
-            className='main-page__name-input'
-            placeholder='Dumbledore'
-            value={playerName}
-            onChange={handleNameChange}
-          />
 
-          <h2 className='main-page__pick-house'>Pick your house</h2>
-          <div className='main-page__houses'>
-            {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map(house => (
-              <div
-                key={house}
-                className='main-page__houses__house'
-                onClick={() => handleHouseSelect(house)}
-                style={{
-                  boxShadow: selectedHouse === house ? '0 4px 40px rgba(0, 0, 0, 0.8)' : 'none',
-                  backgroundColor: selectedHouse === house ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
-                  border: selectedHouse === house ? '1px solid rgb(135, 135, 135)' : 'none',
-                  transform: selectedHouse === house ? 'scale(1.05)' : 'scale(1)',
-                  transition: 'all 0.3s ease-in-out',
-                }}
-              >
-                <img
-                  className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`}
-                  src={`/images/${house}.webp`}
-                  alt={house}
-                />
-                <h3 className='main-page__houses__name'>{house}</h3>
-              </div>
-            ))}
-          </div>
+    <div className={`main-page ${isPlayerInfoLoaded ? "with-background" : ""}`}>
+      <h1 className="main-page__title relative-element">Expelliarmicus</h1>
+      {isPlayerInfoLoaded ? 
+      <>
+        <PlayerBasicInfo playerName={playerName} selectedHouse={selectedHouse} className="fade-in relative-element"/>
+      </>
+      :
+      <div className={"main-page__form relative-element"}>
+        <input 
+          type="text" 
+          className="main-page__name-input" 
+          placeholder="Dumbledore" 
+          value={playerName} 
+          onChange={handleNameChange} 
+        />
+        
+        <h2 className="main-page__pick-house">Pick your house</h2>
+        <div className="main-page__houses">
+          {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
+            <div 
+              key={house} 
+              className="main-page__houses__house"
+              onClick={() => handleHouseSelect(house)}
+              style={{ 
+                boxShadow: selectedHouse === house ? '0 4px 40px rgba(0, 0, 0, 0.8)' : 'none',
+                backgroundColor: selectedHouse === house ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
+                border: selectedHouse === house ? '1px solid rgb(135, 135, 135)' : 'none',
+                transform: selectedHouse === house ? 'scale(1.05)' : 'scale(1)',
+                transition: 'all 0.3s ease-in-out'
+              }}
+              
+            >
+              <img 
+                className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
+                src={`/images/${house}.webp`} 
+                alt={house} 
+              />
+              <h3 className="main-page__houses__name">{house}</h3>
+            </div>
+          ))}
         </div>
-      )}
-      <button
-        className='main-page__button relative-element'
-        onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}
-      >
+      </div>}
+      <button className="main-page__button relative-element" onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}>
+
         {isPlayerInfoLoaded ? 'Jugar' : 'Confirmar'}
       </button>
     </div>
---
+++ src/pages/duelRoom/DuelRoom.jsx
@@ -0,0 +1,13 @@
+import './DuelRoom.scss'
+
+export const DuelRoom = () => {
+    return (
+        <div className="duel-room">
+            <h1 className="duel-room__title relative-element">Duel Room</h1>
+            <div className="duel-room__content relative-element">
+                <p>Welcome to the Duel Room!</p>
+                <p>Prepare for an epic battle!</p>
+            </div>
+        </div>
+    )
+}
