6fcb8f8898a5466b27d1b5fff8514e9afcfc9ad2
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-04-06 13:36:31 +0200

    Feature/name validation (#7)
    * Feature: Add player name validation and error handling
    
    * refactor: Consistent use of single quotes for error state management
    
    * Feature: Add minimum length validation for player name input
    
    * Feature: Add validation for minimum player name length
    
    * refactor: Consistent use of single quotes for error messages in player name validation
    
    * Feature/name-validation: validate button

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,23 +10,40 @@ export const Main = () => {
 
   const [isPlayerInfoLoaded, setIsPlayerInfoLoaded] = useState(false);
 
+  const [error, setError] = useState('');
+
   useEffect(() => {
     const storedPlayer = localStorage.getItem('playerInfo');
     if (storedPlayer) {
       const { name, house } = JSON.parse(storedPlayer);
       setPlayerName(name);
       setSelectedHouse(house);
-      setIsPlayerInfoLoaded(true);
+      setIsPlayerInfoLoaded(false);
     }
   }, []);
   
-  const handleNameChange = (e) => setPlayerName(e.target.value);
-  
-  const handleHouseSelect = (house) => setSelectedHouse(house);
+ 
+  const handleNameChange = e => {
+    /*setPlayerName(e.target.value);*/
+    const value = e.target.value.replace(/[^a-zA-Z]/g, '');
+    setPlayerName(value);
+    setError('');
+    // Clear error when user types
+  };
 
-  const handlePlay = () => {
+  const handleHouseSelect = house => setSelectedHouse(house);
 
-  }
+  const handlePlay = () => {};
+
+  const handleBlur = () => {
+    if (playerName.trim() === '') {
+      setError('The field cannot be empty');
+    } else if (playerName.length < 3) {
+      setError('The value must be at least 3 characters long');
+    } else {
+      setError(''); // Sin errores si la validación pasa
+    }
+  };
   
   const handleConfirm = () => {
     const playerData = { name: playerName, house: selectedHouse };
@@ -37,51 +54,62 @@ export const Main = () => {
   };
 
   return (
+    <div className={`main-page ${isPlayerInfoLoaded ? 'with-background' : ''}`}>
+      <h1 className='main-page__title relative-element'>Expelliarmicus</h1>
+      {isPlayerInfoLoaded ? (
+        <>
+          <PlayerBasicInfo
+            playerName={playerName}
+            selectedHouse={selectedHouse}
+            className='fade-in relative-element'
+          />
+        </>
+      ) : (
+        <div className={'main-page__form relative-element'}>
+          <input
+            type='text'
+            className='main-page__name-input'
+            placeholder='Dumbledore'
+            value={playerName}
+            onChange={handleNameChange}
+            onBlur={handleBlur}
+            minLength={3}
+            maxLength={20}
+          />
 
-    <div className={`main-page ${isPlayerInfoLoaded ? "with-background" : ""}`}>
-      <h1 className="main-page__title relative-element">Expelliarmicus</h1>
-      {isPlayerInfoLoaded ? 
-      <>
-        <PlayerBasicInfo playerName={playerName} selectedHouse={selectedHouse} className="fade-in relative-element"/>
-      </>
-      :
-      <div className={"main-page__form relative-element"}>
-        <input 
-          type="text" 
-          className="main-page__name-input" 
-          placeholder="Dumbledore" 
-          value={playerName} 
-          onChange={handleNameChange} 
-        />
-        
-        <h2 className="main-page__pick-house">Pick your house</h2>
-        <div className="main-page__houses">
-          {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map((house) => (
-            <div 
-              key={house} 
-              className="main-page__houses__house"
-              onClick={() => handleHouseSelect(house)}
-              style={{ 
-                boxShadow: selectedHouse === house ? '0 4px 40px rgba(0, 0, 0, 0.8)' : 'none',
-                backgroundColor: selectedHouse === house ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
-                border: selectedHouse === house ? '1px solid rgb(135, 135, 135)' : 'none',
-                transform: selectedHouse === house ? 'scale(1.05)' : 'scale(1)',
-                transition: 'all 0.3s ease-in-out'
-              }}
-              
-            >
-              <img 
-                className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`} 
-                src={`/images/${house}.webp`} 
-                alt={house} 
-              />
-              <h3 className="main-page__houses__name">{house}</h3>
-            </div>
-          ))}
+          {error && <div style={{ color: 'red' }}>{error}</div>}
+
+          <h2 className='main-page__pick-house'>Pick your house</h2>
+          <div className='main-page__houses'>
+            {['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'].map(house => (
+              <div
+                key={house}
+                className='main-page__houses__house'
+                onClick={() => handleHouseSelect(house)}
+                style={{
+                  boxShadow: selectedHouse === house ? '0 4px 40px rgba(0, 0, 0, 0.8)' : 'none',
+                  backgroundColor: selectedHouse === house ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
+                  border: selectedHouse === house ? '1px solid rgb(135, 135, 135)' : 'none',
+                  transform: selectedHouse === house ? 'scale(1.05)' : 'scale(1)',
+                  transition: 'all 0.3s ease-in-out',
+                }}
+              >
+                <img
+                  className={`main-page__houses__logo main-page__houses__logo--${house.toLowerCase()}`}
+                  src={`/images/${house}.webp`}
+                  alt={house}
+                />
+                <h3 className='main-page__houses__name'>{house}</h3>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>}
-      <button className="main-page__button relative-element" onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}>
-
+      )}
+      <button
+        className='main-page__button relative-element'
+        onClick={isPlayerInfoLoaded ? handlePlay : handleConfirm}
+        disabled={isPlayerInfoLoaded ? false : playerName.length < 3 || selectedHouse === ''}
+      >
         {isPlayerInfoLoaded ? 'Jugar' : 'Confirmar'}
       </button>
     </div>
