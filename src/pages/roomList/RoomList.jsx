2a1728e560276fc523fafe1c89630f90efbfccbb
Alejandro <alejandroluisacosta95@gmail.com>
2025-04-12 12:45:42 +0200

    add: room list initial styles

--- src/pages/roomList/RoomList.jsx
+++ src/pages/roomList/RoomList.jsx
@@ -1,8 +1,25 @@
-export const RoomList = () => {
+import { useContext } from 'react';
+import { StompContext } from '../../stomp/StompProvider';
+import './RoomList.scss';
+
+  export const RoomList = () => {
+    const { rooms } = useContext(StompContext);
     return (
-        <div>
-            <h1>Room List yeah baby</h1>
-            {/* Add your room list logic here */}
+      <div className='room-list'>
+        <h1 className='room-list__title'>¿Listo para el combate?</h1>
+        <div className='room-list__list'>
+        {rooms.map((room, index) => (
+          <div className='room-list__list__room' key={index}>
+            <div className='room-info'>
+            <p className="room-name">{room.name}</p>
+            <p className="players-count">Jugadores: 1/2</p>
+            </div>
+            <div className="play-icon">
+            <span role="img" aria-label="play">▶️</span>
+          </div>
         </div>
-    )
-}
+        ))}
+        </div>
+      </div>
+    );
+  };
