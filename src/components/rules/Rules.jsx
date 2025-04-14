528e3e6ac3561907b3940975c3ed23cab44a8fe5
Alejandro <alejandroluisacosta95@gmail.com>
2025-04-14 18:22:21 +0200

    add: subtitle of app

--- src/components/rules/Rules.jsx
+++ src/components/rules/Rules.jsx
@@ -4,7 +4,7 @@ export const Rules = ({ closeModal }) => {
     return (
         <div className="rules modal-fade-in">
             <img className="rules__close" src="/images/Close.svg" alt="Close" onClick={closeModal}/>
-            <h1 className='rules__title'>El juego donde los verdaderos magos resuelven sus diferencias. ¿Te atreves?</h1>
+            <h1 className='rules__title'>Expelliarmicus™ es el juego donde los verdaderos magos resuelven sus diferencias. ¿Te atreves?</h1>
             <h1 className='rules__rules-label'>Reglas</h1>
             <img className='rules__instructions' src="/images/Instructions.webp" alt="Instructions" />
             <p className='rules__written-instructions'>Tres rondas deciden el destino. Quien conquiste dos, verá su nombre grabado en los pergaminos dorados de Hogwarts. Quien pierda... no dejará ni eco en los pasillos encantados.</p>
